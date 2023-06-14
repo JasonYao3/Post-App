28c4924129c971ac9bb8cb323903fbc1fa069797
Jason Yao <jasonyao105025@gmail.com>
2023-06-13 20:59:41 -0400

    upload files

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@NgModule({
+  // imports: [
+  //   MatInputModule,
+  //   MatButtonModule,
+  //   MatCardModule,
+  //   MatToolbarModule,
+  //   MatExpansionModule,
+  //   MatProgressSpinnerModule,
+  //   MatPaginatorModule,
+  //   MatDialogModule,
+  //   MatFormFieldModule,
+  // ],
+  exports: [
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatDialogModule,
+    MatFormFieldModule,
+  ],
+})
+export class AngularMaterialModule {}
